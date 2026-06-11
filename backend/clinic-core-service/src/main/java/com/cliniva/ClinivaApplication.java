b1bee0de74ca42a8d26ce21bd17419067169c6f2
parth-05122005 <parthkhandelwal1335@gmail.com>
2026-06-11 05:30:35 +0530

    setup: initialize Cliniva backend and Docker infrastructure

---
+++ backend/clinic-core-service/src/main/java/com/cliniva/ClinivaApplication.java
@@ -0,0 +1,15 @@
+package com.cliniva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinivaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                ClinivaApplication.class,
+                args
+        );
+    }
+}
---
+++ backend/clinic-core-service/src/test/java/com/cliniva/ClinicCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliniva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
